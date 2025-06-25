dab80e7959907c74c1c99ef770129302f400505b
Dimplektech <dimplekaware@gmail.com>
2025-06-25 21:28:34 +0100

    Refactor blog templates: replace post_list.html with index.html, update PostList view to use new template, and add custom CSS for styling

--- blog/views.py
+++ blog/views.py
@@ -8,4 +8,5 @@ from django.http import HttpResponse
 class PostList(generic.ListView):
 
     queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginated_by = 6
--- codestar/settings.py
+++ codestar/settings.py
@@ -16,8 +16,12 @@ import dj_database_url
 
 if os.path.isfile('env.py'):
     import env
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 
 # Quick-start development settings - unsuitable for production
@@ -60,7 +64,7 @@ ROOT_URLCONF = 'codestar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
