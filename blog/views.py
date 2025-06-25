2cfc2ada6f3898c87db7c91705d43ca0ed7022db
Dimplektech <dimplekaware@gmail.com>
2025-06-25 10:40:08 +0100

    Update PostList view to filter published posts only

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,5 @@ from django.http import HttpResponse
 # Create your views here.
 class PostList(generic.ListView):
 
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
